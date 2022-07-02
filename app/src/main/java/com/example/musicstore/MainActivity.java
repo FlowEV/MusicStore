d07e6c40779e4cd89144a27729abfa1776630a3a
FlowEV <murpurpower@gmail.com>
2022-07-02 23:40:12 +0300

    Few spinner changes.

--- app/src/main/java/com/example/musicstore/MainActivity.java
+++ app/src/main/java/com/example/musicstore/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.musicstore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void increaseQuantity(View view) {
+        quantity = quantity + 1;
+        TextView quantity = findViewById(R.id.realQuantity);
+        quantity.setText("1");
+    }
+
 }
