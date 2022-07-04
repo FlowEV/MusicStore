f104f044f4f52f5242b4f70a657043a363028c4a
FlowEV <murpurpower@gmail.com>
2022-07-04 23:14:15 +0300

    Spinner Array List added.

--- app/src/main/java/com/example/musicstore/MainActivity.java
+++ app/src/main/java/com/example/musicstore/MainActivity.java
@@ -4,16 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     int quantity = 0;
+    Spinner spinner;
+    ArrayList spinnerArrayList;
+    ArrayAdapter spinnerAdapter;
+
+    HashMap goodsMap;
+    String goodsName;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        spinnerArrayList = new ArrayList();
+
+        spinnerArrayList.add("guitar");
+        spinnerArrayList.add("drums");
+        spinnerArrayList.add("keyboard");
+
+        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,spinnerArrayList);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
+
+        goodsMap = new HashMap();
+        goodsMap.put("guitar", 500);
+        goodsMap.put("drums", 1500);
+        goodsMap.put("keyboard", 1000);
+
     }
 
     public void increaseQuantity(View view) {
@@ -29,5 +59,16 @@ public class MainActivity extends AppCompatActivity {
         }
         TextView decreaseTextView = findViewById(R.id.realQuantity);
         decreaseTextView.setText("" + quantity);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        goodsName = spinner.getSelectedItem().toString();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
