b11ac8d43c0d41c21a8591c237ae369736f81e46
FlowEV <murpurpower@gmail.com>
2022-07-10 21:32:31 +0300

    New interface. Order can be sent by email. Final working version.

--- app/src/main/java/com/example/musicstore/MainActivity.java
+++ app/src/main/java/com/example/musicstore/MainActivity.java
@@ -103,8 +103,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 goodsImageView.setImageResource(R.drawable.guitar);
                 break;
         }
-
-
     }
 
     @Override
@@ -119,17 +117,16 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         order.userName = userNameEditText.getText().toString();
         order.goodsName = goodsName;
         order.quantity = quantity;
+        order.price = price;
         order.orderPrice = quantity * price;
 
         Intent orderIntent = new Intent(MainActivity.this, OrderActivity.class);
         orderIntent.putExtra("userNameForIntent", order.userName);
         orderIntent.putExtra("goodsName", order.goodsName);
         orderIntent.putExtra("quantity", order.quantity);
+        orderIntent.putExtra("price", order.price);
         orderIntent.putExtra("orderPrice", order.orderPrice);
 
-
         startActivity(orderIntent);
-
-
     }
 }
